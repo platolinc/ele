fdfc6a2a4b94ab86cf50d3c969b9739ff3979f81
platolinc <2686550623@qq.com>
2024-01-30 18:37:20 +0800

    feat: 5-20(ScrollBar滚动提示栏组件开发)

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'vant/lib/index.css'
+import './assets/common.scss'
 
 const app = createApp(App)
 
--- src/views/tabs/home/HomeView.vue
+++ src/views/tabs/home/HomeView.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
-import type { IHomeInfo } from '@/types'
+import type { ICountdown, IHomeInfo } from '@/types'
 import TheTop from './components/TheTop.vue'
+import TheTransformer from './components/TheTransformer.vue'
+import ScrollBar from './components/ScrollBar.vue'
 import { useToggle } from '@/use/useToggle'
 import SearchView from '@/views/search/SearchView.vue'
 import { useAsync } from '@/use/useAsync'
 import { fetchHomePageData } from '@/api/home'
 import OpLoadingView from '@/components/OpLoadingView.vue'
 
-const recomments = [
-  {
-    value: 1,
-    label: '牛腩',
-  },
-  {
-    value: 2,
-    label: '色拉',
-  },
-]
-
 const [isSearchViewShown, toggleSearchView] = useToggle(false)
 
-const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
+const { data, pending } = useAsync(fetchHomePageData, {
+  banner: [],
+  searchRecomments: [],
+  transformer: [],
+  scrollBarInfoList: [],
+  countdown: {} as ICountdown,
+  activities: [],
+} as IHomeInfo)
 </script>
 
 <template>
@@ -28,11 +26,13 @@ const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
     <Transition name="fade">
       <SearchView v-if="isSearchViewShown" @cancel="toggleSearchView"></SearchView>
     </Transition>
-    <TheTop :recomments="recomments" @searchClick="toggleSearchView" />
+    <TheTop :recomments="data.searchRecomments" @searchClick="toggleSearchView" />
     <OpLoadingView :loading="pending" type="skeleton">
-      <div>
-        {{ data }}
+      <div class="home-page__banner">
+        <img v-for="v in data.banner" :key="v.imgUrl" :src="v.imgUrl" />
       </div>
+      <TheTransformer :data="data.transformer" />
+      <ScrollBar :data="data.scrollBarInfoList" />
     </OpLoadingView>
   </div>
 </template>
@@ -45,5 +45,17 @@ const { data, pending } = useAsync(fetchHomePageData, {} as IHomeInfo)
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+.home-page {
+  background: var(--op-gray-bg-color);
+  padding-bottom: 70px;
+
+  &__banner {
+    img {
+      width: 100%;
+      padding-top: 10px;
+      background: white;
+    }
+  }
 }
 </style>
