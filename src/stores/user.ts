b8ee0f5408bea10420d426969626386ae4c6172b
platolinc <2686550623@qq.com>
2024-02-03 21:40:56 +0800

    feat: 自定义hooks-useLocalStorage 保存用户状态信息

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import type { IUserInfo } from '@/types'
+import { useLocalStorage } from '@/use/useLocalStorage'
 
 export interface IUserState {
   userInfo: IUserInfo
@@ -14,27 +15,39 @@ const getDefaultUserInfo: () => IUserInfo = () => ({
 })
 
 export const useUserStore = defineStore('user', () => {
+  const {
+    value: $userInfo,
+    setValue: $setUserInfoValue,
+    removeItem: $removeUserInfoItem,
+  } = useLocalStorage('userInfo', getDefaultUserInfo())
+  const { setValue: $setTokenValue, removeItem: $removeTokenItem } = useLocalStorage(
+    'token',
+    getDefaultUserInfo(),
+  )
+
   const state = ref({
     userInfo: getDefaultUserInfo(),
     token: '',
   })
   const getUserInfo = computed(() => {
     // 为什么不直接读 localStorage 的值呢？
-    // 因为读取 localStorage 是比较耗时的操作，所以这里先读 store
-    // if (!state.value.userInfo || !state.value.userInfo.id) {
-    //   state.value.userInfo = $userInfo.value
-    // }
+    // 因为读取 localStorage 是比较耗时的操作，所以优先读 store， 没有值的话读 localStorage
+    if (!state.value.userInfo || !state.value.userInfo.id) {
+      state.value.userInfo = $userInfo.value
+    }
     return state.value.userInfo
   })
   const setInfo = ({ token, userInfo }: IUserState) => {
     state.value.userInfo = userInfo
     state.value.token = token
+    $setUserInfoValue(userInfo)
+    $setTokenValue(token)
   }
   const removeInfo = () => {
     state.value.userInfo = getDefaultUserInfo()
     state.value.token = ''
-    // $removeUserInfoItem()
-    // $removeTokenItem()
+    $removeUserInfoItem()
+    $removeTokenItem()
   }
 
   return { state, getUserInfo, setInfo, removeInfo }
