2df9e618d82348b0329a3749e25651e7cf5ecb91
platolinc <2686550623@qq.com>
2024-02-03 22:28:45 +0800

    feat: 实现请求携带token

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,21 +1,42 @@
 import axios from 'axios'
-import { Dialog } from 'vant'
+import { showDialog } from 'vant'
+import { useLocalStorage } from '../use/useLocalStorage'
 
 const instance = axios.create({
   baseURL: '/api',
 })
 
-instance.interceptors.response.use((response) => {
-  const { data: _data } = response
-  const { data, code, msg } = _data
-  if (code !== 0) {
-    Dialog.alert({
-      message: msg,
-    }).then(() => {
-      // 关闭弹窗的逻辑
-    })
-    return Promise.reject(msg)
+instance.interceptors.request.use((config) => {
+  const { value: token } = useLocalStorage('token', '')
+  if (config.headers && token.value) {
+    config.headers['x-token'] = token.value
   }
-  return data
+  return config
 })
+
+instance.interceptors.response.use(
+  (response) => {
+    const { data: _data } = response
+    const { data, code, msg } = _data
+    if (code !== 0) {
+      showDialog({
+        message: msg,
+      }).then(() => {
+        // 关闭弹窗的逻辑
+      })
+      return Promise.reject(msg)
+    }
+    return data
+  },
+  (err) => {
+    if (err.response && err.response.status === 401) {
+      showDialog({
+        message: '请登录',
+      }).then(() => {
+        // 关闭弹窗的逻辑
+      })
+    }
+  },
+)
+
 export default instance
