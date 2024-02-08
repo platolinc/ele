57eb6d6f2fb2bed1e834c5678c21f9ec21a21477
platolinc <2686550623@qq.com>
2024-02-08 15:31:10 +0800

    feat: 自定义hooks-useLifeHook 解决非 setup 调用生命周期 hooks 问题

---
+++ src/use/useLifeHook.ts
@@ -0,0 +1,33 @@
+import { onUnmounted as _onUnmounted } from 'vue'
+
+type Hook = () => void
+type LifeHook = (hook: Hook) => unknown
+interface IHookItem {
+  hooks: LifeHook
+  queue: Hook[]
+}
+const hookMap: Record<string, IHookItem> = {
+  onUnmounted: {
+    hooks: _onUnmounted,
+    queue: [],
+  },
+}
+
+export function useLifeHook() {
+  const keys = Object.keys(hookMap)
+  return keys.reduce(
+    (p, key) => {
+      const hookItem = hookMap[key]
+      hookItem.hooks(() => {
+        hookItem.queue.forEach((v) => v())
+      })
+      return {
+        ...p,
+        [key]: (hook: Hook) => {
+          hookItem.queue.push(hook)
+        },
+      }
+    },
+    {} as Record<string, LifeHook>,
+  )
+}
