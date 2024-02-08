81bf53b33990c34482fcb93a9502a763e4755aad
platolinc <2686550623@qq.com>
2024-02-08 21:37:11 +0800

    fix: 修复引入路径的错误

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import HomeView from '../views/tabs/home/HomeView.vue'
 import OrderView from '../views/tabs/order/OrderView.vue'
 import MeView from '../views/tabs/me/MeView.vue'
 import LoginView from '../views/login/LoginView.vue'
-import ShopView from '../views/shop/shopView.vue'
+import ShopView from '../views/shop/ShopView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
